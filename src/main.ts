be580ff7507c3d6011615025a2e0b5fd4a5f0590
Vo Tuan Di <di_vo@mediconcen.com>
2024-02-26 14:55:57 +0700

    update docker

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,25 @@ import { ValidationPipe } from '@nestjs/common'
 // import * as cors from 'cors'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import path from 'path'
+import * as fs from 'fs'
+import * as https from 'https'
 
 declare const module: any
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('./path/to/private.key'),
+      cert: fs.readFileSync('./path/to/certificate.crt'),
+    },
+  })
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
+  // app.use(cors)
   app.useStaticAssets(path.join(__dirname, '..', 'public'), {
     prefix: '/public/',
   })
